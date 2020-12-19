93b2d89fedff965a7fd7d0754dc5f9f75c0a2cb3
Niyoko Yuliawan <niyoko.yuliawan@logisly.com>
2020-12-20 02:36:41 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const AssetsPlugin = require('assets-webpack-plugin');
+const SriPlugin = require('webpack-subresource-integrity');
+
+module.exports = {
+  mode: 'production',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]-bundle-[contenthash].js',
+    publicPath: '/js/',
+  },
+  plugins: [
+    new AssetsPlugin({
+      path: path.join(__dirname, 'dist'),
+      prettyPrint: true,
+      integrity: true,
+    }),
+    new SriPlugin({
+      hashFuncNames: ['sha256', 'sha384'],
+      enabled: true,
+    }),
+  ],
+};
